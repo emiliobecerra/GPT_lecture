a23cc01e62f888778f9f6812de7741ad64232939
Emilio Becerra <emiliobecerra@150.216.21.198.tigernet.wifi.dyn.clemson.edu>
2024-04-25 11:23:19 -0400

    first commit

---
+++ evaluate_reviews.py
@@ -0,0 +1,32 @@
+# read csv file from dataset.csv
+
+# import pandas
+
+# data = pandas.read_csv('dataset.csv')
+
+from dotenv import load_dotenv
+
+#For openai's package 
+load_dotenv()
+
+from openai import OpenAI
+client = OpenAI()
+
+def simple_call(prompt):
+	completions = client.chat.completions.create(model = "gpt-3.5-turbo", 
+		messages=[
+	#System role, adding context for the question
+	#{"role": "system", "content": "You are a helpful assistant"},
+	#Not including the system role will assume the gpt is your assistant
+		{"role": "user", "content": prompt},
+		], max_tokens=200
+	# temperature how creative it is, but higher temperature results in really creative begining content, but incomprehensible words later. 
+		, temperature=0.1
+	#top_p: from the highest probability of a word to a lower probability, top_p will restrict to certain words depending on the probability threshold. Default is 1
+		, top_p=1
+
+		)
+	return completions.choices[0].message.content
+
+result = simple_call("What is the capital of the United States")	
+print(result)
